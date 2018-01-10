5565b8625d24ae9d2008c9d50543505ac02e32e5
jBrabs97 <35259783+jBrabs97@users.noreply.github.com>
2018-01-10 12:57:58 +0000

    second

--- js/javascript.js
+++ js/javascript.js
@@ -12,14 +12,18 @@ function Init() {
 function showDiv(select) {
     if (select.value == blue) {
         document.getElementById('jersey1').style.display = "block";
+        console.log('blue show')
     }
     if (select.value == green) {
         document.getElementById('jersey2').style.display = "block";
+        console.log('green show')
     }
     if (select.value == red) {
         document.getElementById('jersey3').style.display = "block";
+        console.log('red show')
     }
     if (select.value == turquoise) {
         document.getElementById('jersey4').style.display = "block";
+        console.log('turqouise show')
     }
 }

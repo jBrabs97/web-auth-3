2feacbe415c4e075188ddda30814ae9462747a50
jBrabs97 <35259783+jBrabs97@users.noreply.github.com>
2018-01-10 14:53:44 +0000

    third

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,7 @@
 // JavaScript Document
+
+
+src="https://code.jquery.com/jquery-1.12.4.min.js"
 window.onload = function () {
     console.log('loaded');
     Init();
@@ -7,23 +10,80 @@ window.onload = function () {
 
 
 function Init() {
+    console.log('loaded2');
 }
 
-function showDiv(select) {
-    if (select.value == blue) {
-        document.getElementById('jersey1').style.display = "block";
-        console.log('blue show')
-    }
-    if (select.value == green) {
-        document.getElementById('jersey2').style.display = "block";
-        console.log('green show')
-    }
-    if (select.value == red) {
-        document.getElementById('jersey3').style.display = "block";
-        console.log('red show')
-    }
-    if (select.value == turquoise) {
-        document.getElementById('jersey4').style.display = "block";
-        console.log('turqouise show')
-    }
-}
+
+
+
+
+$(document).ready(function () {
+    $("select").change(function () {
+        $(this).find("option:selected").each(function () {
+            var optionValue = $(this).attr("value");
+            if (optionValue) {
+                $(".box").not("." + optionValue).hide();
+                $("." + optionValue).show();
+            } else {
+                $(".box").hide();
+            }
+        });
+    }).change();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+//function showDiv(select) {
+//    if (select.value == "blue") {
+//        document.getElementById('jersey1').style.display = "block";
+//        console.log('blue show')
+//    }
+//    if (select.value == "green") {
+//        document.getElementById('jersey2').style.display = "block";
+//        console.log('green show')
+//    }
+//    if (select.value == "red") {
+//        document.getElementById('jersey3').style.display = "block";
+//        console.log('red show')
+//    }
+//    if (select.value == "turquoise") {
+//        document.getElementById('jersey4').style.display = "block";
+//        console.log('turqouise show')
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    $("select").change(function () {
+        $(this).find("option:selected").each(function () {
+            var optionValue = $(this).attr("value");
+            if (optionValue) {
+                $(".box").not("." + optionValue).hide();
+                $("." + optionValue).show();
+            } else {
+                $(".box").hide();
+            }
+        });
+    }).change();
+});

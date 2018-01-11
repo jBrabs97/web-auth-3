02f8f1737a7389ef3d339ba96a9c37c927ec175a
jBrabs97 <35259783+jBrabs97@users.noreply.github.com>
2018-01-11 14:44:01 +0000

    eight

--- js/javascript.js
+++ js/javascript.js
@@ -1,51 +1,92 @@
 // JavaScript Document
 
-
-
-window.onload = function () {
-    console.log('loaded');
-    Init();
-}
-
-
-
-function Init() {
-    console.log('loaded2');
-}
-
-
-
-
-
-
-
-
-
-
-
-
-function showDiv(select) {
-    if (select.value == "blue") {
-        document.getElementById('jersey1').style.display = "block";
+function showJersey(jersey) {
+    if (jersey == 'blue') {
         console.log('blue show')
+        document.getElementById('jersey1').style.display = 'block';
+        document.getElementById('jersey2').style.display = 'none';
+        document.getElementById('jersey3').style.display = 'none';
+        document.getElementById('jersey4').style.display = 'none';
     }
-    if (select.value == "green") {
-        document.getElementById('jersey2').style.display = "block";
-        console.log('green show')
+    if (jersey == 'green') {
+        document.getElementById('jersey1').style.display = 'none';
+        document.getElementById('jersey2').style.display = 'block';
+        document.getElementById('jersey3').style.display = 'none';
+        document.getElementById('jersey4').style.display = 'none';
+     
     }
-    if (select.value == "red") {
-        document.getElementById('jersey3').style.display = "block";
-        console.log('red show')
+    if (jersey == 'red') {
+        document.getElementById('jersey1').style.display = 'none';
+        document.getElementById('jersey2').style.display = 'none';
+        document.getElementById('jersey3').style.display = 'block';
+        document.getElementById('jersey4').style.display = 'none';
+      
     }
-    if (select.value == "turquoise") {
-        document.getElementById('jersey4').style.display = "block";
-        console.log('turqouise show')
+    if (jersey == 'turqouise') {
+        document.getElementById('jersey1').style.display = 'none';
+        document.getElementById('jersey2').style.display = 'none';
+        document.getElementById('jersey3').style.display = 'none';
+        document.getElementById('jersey4').style.display = 'block';
+        
+    }
+}
+
+function showLogo(logo) {
+    if (logo == 'stars') {
+        document.getElementById('logo1').style.display = 'block';
+        document.getElementById('logo2').style.display = 'none';
+        document.getElementById('logo3').style.display = 'none';
+        document.getElementById('logo4').style.display = 'none';
+    }
+    if (logo == 'penguins') {
+        document.getElementById('logo1').style.display = 'none';
+        document.getElementById('logo2').style.display = 'block';
+        document.getElementById('logo3').style.display = 'none';
+        document.getElementById('logo4').style.display = 'none';
+    }
+    if (logo == 'jets') {
+        document.getElementById('logo1').style.display = 'none';
+        document.getElementById('logo2').style.display = 'none';
+        document.getElementById('logo3').style.display = 'block';
+        document.getElementById('logo4').style.display = 'none';
+    }
+    if (logo == 'sharks') {
+        document.getElementById('logo1').style.display = 'none';
+        document.getElementById('logo2').style.display = 'none';
+        document.getElementById('logo3').style.display = 'none';
+        document.getElementById('logo4').style.display = 'block';
     }
 }
 
 
 
 
+
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', function (event) {
+    var colorSelect = document.getElementById('color_select');
+    var logoSelect = document.getElementById('logo_select');
+
+
+    colorSelect.addEventListener('change', function (event) {
+        console.log(this.value);
+        showJersey(this.value);
+    })
+
+    logoSelect.addEventListener('change', function (event) {
+        console.log(this.value);
+        showLogo(this.value);
+    })
+
+
+
+
+
+});
 
 
 
